8d4a2a4a79167ae365da33a2e4599522537ec55f
Yuri D. Lenskiy <yuri.lenskiy@gmail.com>
2011-03-21 00:55:15 -0400

    Made xpdf default emacs pdf previewer.

--- emacs.d/settings-auctex.el
+++ emacs.d/settings-auctex.el
@@ -5,9 +5,16 @@
 
 ; setup parsing
 (setq TeX-parse-self t)
-(setq TeX-auto-save t)
 
 (setq TeX-auto-untabify t)
 
 (setq LaTeX-amsmath-label "eq:")
 (add-hook 'LaTeX-mode-hook 'flyspell-mode)
+
+;; setup xpdf pdf viewing by default
+(add-hook 'LaTeX-mode-hook
+          '(lambda ()
+             (setq TeX-view-program-selection
+                   (add-to-list
+                    (remove '(output "Evince") 'TeX-view-program-selection)
+                    '(output-pdf "xpdf")))))
