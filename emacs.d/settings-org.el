b01816419844e3ac0f6a1ef1c4a0bc4077482cbd
Yuri D. Lenskiy <yuri.lenskiy@gmail.com>
2011-04-17 00:32:33 -0400

    Added default tags and refile targets to org-mode.

--- emacs.d/settings-org.el
+++ emacs.d/settings-org.el
@@ -1,3 +1,14 @@
 ;; Org-mode
 (eval-after-load "org" '(add-to-list 'org-agenda-files "~/notes/"))
+(setq org-refile-targets '(("~/notes/main.org" :maxlevel . 3)))
 (setq org-log-done 'time)
+(setq org-tag-persistent-alist
+      '(("@phone" . ?p)
+        ("@computer" . ?c)
+        ("school" . ?s)
+        ("french" . ?f)
+        ("english" . ?e)
+        ("african" . ?a)
+        ("internship" . ?i)
+        ("scholarships")
+        ("someday")))
